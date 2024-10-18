a98eaca43053635e0e4a00adef71387c65df44f6
mcgrodk <mcgroderk@gmail.com>
2024-10-17 21:02:55 -0700

    Updated HTML IDs and reorganized enemies; battle functions in progress

--- script.js
+++ script.js
@@ -14,8 +14,12 @@ const textContainer = document.querySelector("#text-container");
 let exp = 0;
 let confidence = 100;
 let cred = 500; // Default 100; Value raised temporarily for testing
+let text1 = document.querySelector("#text1");
+let text2 = document.querySelector("#text2");
+let enemy;
 let enemyName = document.querySelector("#enemy-name");
-let enemyBugs = document.querySelector("enemy-bugs");
+let bugsText = document.querySelector("#bugs-text");
+let enemyBugs;
 
 let inventory = [
   {
@@ -47,7 +51,6 @@ const locations = [
     "name": "battle mode",
     "button text": ["Attack", "Think", "Run"],
     "button function": [attack, guard, goHome],
-    "text": "Filler text."
   },
 ]
 
@@ -70,47 +73,43 @@ let shopItems = [
   }
 ]
 
-let easyEnemies = [
+let enemies = [
   {
     "name": "Weather App",
     "bugs": 20,
-    "power": 10,
+    "power": 8,
   },
   {
     "name": "Rock Paper Scissors",
-    "bugs": 20,
-    "power": 10,
+    "bugs": 10,
+    "power": 4,
   },
   {
     "name": "To-Do List",
-    "bugs": 20,
-    "power": 10
-  }
-];
-
-let hardEnemies = [
+    "bugs": 15,
+    "power": 6,
+  },
   {
     "name": "Message Board",
-    "bugs": 100,
-    "power": 20,
+    "bugs": 80,
+    "power": 15,
   },
   {
     "name": "Shopping Cart",
-    "bugs": 100,
-    "power": 20,
+    "bugs": 50,
+    "power": 11,
   },
   {
     "name": "Tic Tac Toe",
-    "bugs": 100,
-    "power": 20
+    "bugs": 60,
+    "power": 12
+  },
+  {
+  "name": "5-D Chess in Space with Crypto Betting",
+  "bugs": 200,
+  "power": 20
   }
-];
-
-let interviewBoss = {
-  "name": "\n5-D Chess in Space with Crypto Betting",
-  "bugs": 300,
-  "power": 30
-};
+]
 
 button1.onclick = goShop;
 button2.onclick = goCode;
@@ -123,7 +122,8 @@ function update(location) {
   button1.textContent = location["button text"][0];
   button2.textContent = location["button text"][1];
   button3.textContent = location["button text"][2];
-  textContainer.textContent = location["text"];
+  text1.textContent = location["text"];
+  text2.style.display = "none";
   button1.onclick = location["button function"][0];
   button2.onclick = location["button function"][1];
   button3.onclick = location["button function"][2];
@@ -144,43 +144,45 @@ function goCode() {
 function battleEasy() {
   update(locations[3]);
   enemyStats.style.display = "flex";
-  enemy = easyEnemies[Math.floor(Math.random() * 3)]; // Selects random index from easyEnemies array
+  enemy = enemies[Math.floor(Math.random() * 3)]; // Selects easy enemy of index 0-2 from enemies array
   enemyName.textContent = "\n" + enemy["name"];
   enemyBugs = enemy["bugs"]
-  enemyBugs.textContent = enemyBugs;
-  textContainer.textContent = "Now coding " + enemy["name"] + ". Squash those bugs!";
+  bugsText.textContent = '\n' + enemyBugs;
+  text1.textContent = "Now coding " + enemy["name"] + ". Squash those bugs!";
 }
 
 function battleHard() {
   update(locations[3]);
   enemyStats.style.display = "flex";
-  enemy = hardEnemies[Math.floor(Math.random() * 3)]; // Selects random index from hardEnemies array
+  enemy = enemies[Math.floor(Math.random() * (6 - 3)) + 3]; // Selects hard enemy of index 3-5 from enemies array
   enemyName.textContent = "\n" + enemy["name"];
   enemyBugs = enemy["bugs"]
-  enemyBugs.textContent = enemyBugs;
-  textContainer.textContent = "Now coding " + enemy["name"] + ". Squash those bugs!";
+  bugsText.textContent = '\n' + enemyBugs;
+  text1.textContent = "Now coding " + enemy["name"] + ". Squash those bugs!";
 }
 
 function goInterview() {
   update(locations[3])
   enemyStats.style.display = "flex";
-  textContainer.textContent = "The dreaded job interview code challenge! This one's a real monster!"
-  enemyName.textContent = interviewBoss["name"];
-  enemyBugs = interviewBoss["bugs"];
-  enemyBugs.textContent = enemyBugs;
+  enemy = enemies[6];
+  text1.textContent = "The dreaded job interview code challenge! This one's a real monster!"
+  enemyName.textContent = '\n' + enemy["name"];
+  enemyBugs = enemy["bugs"];
+  bugsText.textContent = '\n' + enemyBugs;
 }
 
 // Shop functions
 
 function buyConfidence() {
+  text2.style.display = "none";
   if (cred < 10) {
-    textContainer.textContent = "Not enough cred!";
+    text1.textContent = "Not enough cred!";
   } else {
   cred -= 10;
   credText.textContent = cred;
   confidence += 10;
   confidenceText.textContent = confidence;
-  textContainer.textContent = "You bought 10 confidence. (Don't question how that works.)";
+  text1.textContent = "You bought 10 confidence. (Don't question how that works.)";
 }
 }
 
@@ -188,19 +190,18 @@ function buyGear() {
   if (shopItems.length !== 0) {
 
   if (cred < 30) {
-    textContainer.textContent = "Not enough cred!";
+    text1.textContent = "Not enough cred!";
   } else {
   cred -= 30;
   credText.textContent = cred;
   inventory.push(shopItems[0]);
   shopItems.shift();
-  textContainer.textContent = "You bought " + 
-  inventory[inventory.length -1]["item name"] + ".";
+  text1.textContent = "You bought " + inventory[inventory.length -1]["item name"] + ".";
   showInventory();
 }
 
   } else {
-    textContainer.textContent = "Shop is all sold out! Wnat to sell?";
+    text1.textContent = "Shop is all sold out! Want to sell?";
     button2.textContent = "Sell gear (30 cred)";
     button2.onclick = sellGear;
   }
@@ -208,9 +209,9 @@ function buyGear() {
 
 function sellGear() {
   if (inventory.length === 1) {
-    textContainer.textContent = "Don't sell your only item!";
+    text1.textContent = "Don't sell your only item!";
   } else {
-    textContainer.textContent = "You sold " + inventory[0]["item name"] + " for 30 cred.";
+    text1.textContent = "You sold " + inventory[0]["item name"] + " for 30 cred.";
     shopItems.push(inventory[0]);
     inventory.shift();
     showInventory();
@@ -218,11 +219,12 @@ function sellGear() {
 }
 
 function showInventory() {
-  textContainer.textContent += "\nCurrent inventory: "
+  text2.style.display = "block";
+  text2.textContent = "\nCurrent inventory: "
   for (i = 0; i < inventory.length; i++) {
-    textContainer.textContent += inventory[i]["item name"]
+    text2.textContent += inventory[i]["item name"]
     if (i != inventory.length - 1) { // Neatly formats inventory listing so that all but last item are followed by ", "
-      textContainer.textContent += ", "
+      text2.textContent += ", "
     }
   }
  }
@@ -230,7 +232,18 @@ function showInventory() {
 // Battle mechanics functions
 
 function attack() {
-  
+  let attackPower = 0;
+  let enemyAttackPower = 0;
+  for (let i = 0; i < inventory.length; i++) {
+    attackPower += inventory[i]["power"];
+  }
+  attackPower += Math.floor(Math.random() * 4)
+  enemyBugs -= attackPower;
+  bugsText.textContent = enemyBugs;
+  enemyAttackPower += enemy["power"] + Math.floor(Math.random() * 4)
+  confidence -= enemyAttackPower;
+  confidenceText.textContent = confidence;
+  text1.textContent = `You fixed ${attackPower} bugs and took ${enemyAttackPower} damage to confidence.`  
 }
 
 function guard() {
