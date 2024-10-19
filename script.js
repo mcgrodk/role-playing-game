3efed425fefc306966728396b2f3d3ea48e35aa5
mcgrodk <mcgroderk@gmail.com>
2024-10-18 21:17:46 -0700

    Added functions, redefined variables; first fully playable game commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Battle sequenes starting, but bugs text not displaying
-// ToDo: Simplify enemy array of objects into one object for easyEnemies,
-// one object for hardEnemies with an array just for the names?
+// TODO: Factory player XP into damage dealt/received in battle
+// TODO: Update strings to template literals, objects to dot notation
 
 const button1 = document.querySelector("#button-1");
 const button2 = document.querySelector("#button-2");
@@ -9,16 +8,16 @@ const expText = document.querySelector("#exp-text");
 const confidenceText = document.querySelector("#confidence-text");
 const credText = document.querySelector("#cred-text");
 const enemyStats = document.querySelector("#enemy-stats");
+const enemyName = document.querySelector("#enemy-name");
+const bugsText = document.querySelector("#bugs-text");
 const textContainer = document.querySelector("#text-container");
+const text1 = document.querySelector("#text1");
+const text2 = document.querySelector("#text2");
 
 let exp = 0;
 let confidence = 100;
-let cred = 500; // Default 100; Value raised temporarily for testing
-let text1 = document.querySelector("#text1");
-let text2 = document.querySelector("#text2");
+let cred = 50;
 let enemy;
-let enemyName = document.querySelector("#enemy-name");
-let bugsText = document.querySelector("#bugs-text");
 let enemyBugs;
 
 let inventory = [
@@ -37,7 +36,7 @@ const locations = [
   },
   {
     "name": "shop",
-    "button text": ["Buy confidence (10 cred)", "Buy gear (30 cred)", "Log out"],
+    "button text": ["Buy confidence (10 cred)", "Buy gear (30 cred)", "Log off"],
     "button function": [buyConfidence, buyGear, goHome],
     "text": "You log onto the web store."
   },
@@ -50,8 +49,18 @@ const locations = [
   {
     "name": "battle mode",
     "button text": ["Attack", "Think", "Run"],
-    "button function": [attack, guard, goHome],
+    "button function": [attack, think, goHome],
   },
+  {
+    "name": "post-battle",
+    "button text": ["Log off", "Log off", "Log off"],
+    "button function": [goHome, goHome, goHome]
+  },
+  {
+    "name": "play again",
+    "button text": ["Play again?", "Play again?", "Play again?"],
+    "button function": [reset, reset, reset]
+  }
 ]
 
 let shopItems = [
@@ -64,7 +73,7 @@ let shopItems = [
     "power": 10
   },
   {
-    "item name": "Nerd Glasses",
+    "item name": "RGB Backlit Keyboard",
     "power": 20,
   },
   {
@@ -145,9 +154,9 @@ function battleEasy() {
   update(locations[3]);
   enemyStats.style.display = "flex";
   enemy = enemies[Math.floor(Math.random() * 3)]; // Selects easy enemy of index 0-2 from enemies array
-  enemyName.textContent = "\n" + enemy["name"];
+  enemyName.textContent = enemy["name"];
   enemyBugs = enemy["bugs"]
-  bugsText.textContent = '\n' + enemyBugs;
+  bugsText.textContent = enemyBugs;
   text1.textContent = "Now coding " + enemy["name"] + ". Squash those bugs!";
 }
 
@@ -155,9 +164,9 @@ function battleHard() {
   update(locations[3]);
   enemyStats.style.display = "flex";
   enemy = enemies[Math.floor(Math.random() * (6 - 3)) + 3]; // Selects hard enemy of index 3-5 from enemies array
-  enemyName.textContent = "\n" + enemy["name"];
+  enemyName.textContent = enemy["name"];
   enemyBugs = enemy["bugs"]
-  bugsText.textContent = '\n' + enemyBugs;
+  bugsText.textContent = enemyBugs;
   text1.textContent = "Now coding " + enemy["name"] + ". Squash those bugs!";
 }
 
@@ -165,10 +174,10 @@ function goInterview() {
   update(locations[3])
   enemyStats.style.display = "flex";
   enemy = enemies[6];
-  text1.textContent = "The dreaded job interview code challenge! This one's a real monster!"
-  enemyName.textContent = '\n' + enemy["name"];
+  enemyName.textContent = enemy["name"];
   enemyBugs = enemy["bugs"];
-  bugsText.textContent = '\n' + enemyBugs;
+  bugsText.textContent = enemyBugs;
+  text1.textContent = "The dreaded job interview code challenge! This one's a real monster!"
 }
 
 // Shop functions
@@ -183,7 +192,7 @@ function buyConfidence() {
   confidence += 10;
   confidenceText.textContent = confidence;
   text1.textContent = "You bought 10 confidence. (Don't question how that works.)";
-}
+  }
 }
 
 function buyGear() {
@@ -220,7 +229,7 @@ function sellGear() {
 
 function showInventory() {
   text2.style.display = "block";
-  text2.textContent = "\nCurrent inventory: "
+  text2.textContent = "Current inventory: "
   for (i = 0; i < inventory.length; i++) {
     text2.textContent += inventory[i]["item name"]
     if (i != inventory.length - 1) { // Neatly formats inventory listing so that all but last item are followed by ", "
@@ -231,21 +240,110 @@ function showInventory() {
 
 // Battle mechanics functions
 
-function attack() {
+function getAttackPower() {
   let attackPower = 0;
-  let enemyAttackPower = 0;
   for (let i = 0; i < inventory.length; i++) {
-    attackPower += inventory[i]["power"];
+    attackPower += inventory[i]["power"]
   }
-  attackPower += Math.floor(Math.random() * 4)
-  enemyBugs -= attackPower;
-  bugsText.textContent = enemyBugs;
-  enemyAttackPower += enemy["power"] + Math.floor(Math.random() * 4)
-  confidence -= enemyAttackPower;
-  confidenceText.textContent = confidence;
-  text1.textContent = `You fixed ${attackPower} bugs and took ${enemyAttackPower} damage to confidence.`  
+  attackPower += Math.floor(Math.random() * 4);
+  return attackPower;
 }
 
-function guard() {
-  
+function getEnemyAttackPower() {
+  let enemyAttackPower = enemy["power"]
+  enemyAttackPower += Math.floor(Math.random() * 4);
+  return enemyAttackPower;
+}
+
+function attack() {
+  attackPower = getAttackPower();
+  enemyBugs -= attackPower;
+  bugsText.textContent = enemyBugs;
+  enemyAttackPower = getEnemyAttackPower();
+ 
+  confidence -= enemyAttackPower;
+  confidenceText.textContent = confidence;
+
+  if (confidence <= 0) {
+    gameOver();
+  } else if (enemyBugs <= 0) {
+    if (enemy == enemies[6]) { // Checks if enemy is interview boss
+      winGame();
+    } else {
+      winBattle();
+    }
+  } else {
+    text1.textContent = `You fixed ${attackPower} bugs and took ${enemyAttackPower} damage to confidence.`  
+  }
+}
+
+function think() {
+  enemyAttackPower = Math.floor(getEnemyAttackPower() / 2);
+  confidence -= enemyAttackPower;
+  confidenceText.textContent = confidence;
+  text1.textContent = `You pause to consider a solution to the problem. Took a reduced ${enemyAttackPower} damage to confidence.`;
+}
+
+function winBattle() {
+  update(locations[4]);
+  text2.style.display = "none";
+  cred += enemy["power"] * (Math.floor(Math.random() * 4) + 2);
+  credText.textContent = cred;
+  exp += enemy["power"];
+  expText.textContent = exp;
+  text1.textContent = `You completed the code challenge! Earned ${enemy["power"]} experience and some cred.`;
+}
+
+function winGame() {
+  update(locations[5]);
+  enemyStats.style.display = "none";
+  text1.textContent = "You pass the code interview, advance through six more stages of interviewing, and receive a job offer! CONGRATULATIONS!";
+  text2.style.display = "block";
+  text2.textContent = "Thanks for playing!";
+}
+
+function gameOver() {
+  update(locations[5]);
+  text1.textContent = "You lost all confidence! Your dreams of becoming a professional programmer are crushed.";
+  text2.style.display = "block";
+  text2.style["font-weight"] = "bold";
+  text2.textContent = "G A M E O V E R";
+}
+
+function reset() {
+  text2.style["font-weight"] = "normal";
+  exp = 0;
+  expText.textContent = exp;
+  confidence = 100;
+  confidenceText.textContent = confidence;
+  cred = 50;
+  credText.textContent = cred;
+
+  inventory = [
+    {
+      "item name": "Raspberry Pi",
+      "power": 5
+    }
+  ]
+
+  shopItems = [
+    {
+      "item name": "Dual Monitor",
+      "power": 5
+    },
+    {
+      "item name": "Gamer Chair",
+      "power": 10
+    },
+    {
+      "item name": "RGB Backlit Keyboard",
+      "power": 20,
+    },
+    {
+      "item name": "BS in CS",
+      "power": 30
+    }
+  ]
+
+  goHome();
 }
